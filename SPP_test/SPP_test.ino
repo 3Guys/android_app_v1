7e2392e442a657bb6ed5555b3d61dac6f178fde0
bakazangetsu <llehotog_22@hotmail.com>
2013-08-20 16:46:21 -0400

    code works with blueterm
    uses blueterm and ascii values of the keyboard to drive the servo

--- SPP_test/SPP_test.ino
+++ SPP_test/SPP_test.ino
@@ -6,6 +6,7 @@
 
 #include <SPP.h>
 #include <usbhub.h>
+#include <Servo.h>
 
 USB Usb;
 USBHub Hub1(&Usb); // Some dongles have a hub inside
@@ -13,6 +14,8 @@ BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
 /* You can create the instance of the class in two ways */
 SPP SerialBT(&Btd); // This will set the name to the defaults: "Arduino" and the pin to "1234"
 //SPP SerialBT(&Btd, "Lauszus's Arduino","0000"); // You can also set the name and pin like so
+int incomingbyte;
+Servo servo1;
 
 boolean firstMessage = true;
 
@@ -23,20 +26,52 @@ void setup() {
     while(1); //halt
   }
   Serial.print(F("\r\nSPP Bluetooth Library Started"));
+  pinMode(9, OUTPUT);
+  servo1.attach(9);
+  
 }
 void loop() {
   Usb.Task(); // The SPP data is actually not send until this is called, one could call SerialBT.send() directly as well
   
   if(SerialBT.connected) {
     if(firstMessage) {
+      Serial.println("check3");
       firstMessage = false;
       SerialBT.println(F("Hello from Arduino")); // Send welcome message
+      //servo1.write(map(Serial.read(), 0, 255, 0, 180));
+      //servo1.write(Serial.read());
     }
-    if(Serial.available())
-      SerialBT.write(Serial.read());
-    if(SerialBT.available())
-      Serial.write(SerialBT.read());
+    if(Serial.available()){
+      Serial.println("check1");
+      incomingbyte=(Serial.read());
+      //SerialBT.write(Serial.read());
+      servo1.write(incomingbyte);
+    }
+    if(SerialBT.available()){
+      Serial.println("check2");
+      //Serial.write(Serial.write((SerialBT.read())));  //write to the monitor
+      incomingbyte=(SerialBT.read());
+      servo1.write(incomingbyte);
+    }
   } 
   else 
     firstMessage = true;
+
+  if(SerialBT.available()) {
+    Serial.println("goes into write bracket");
+    //servo1.write(SerialBT.read());
+   // servo2.write(map(PS3.getAnalogHat(LeftHatY), 0, 255, 180, 0));
+  }
+  else 
+   {
+     //Serial.println("check5");
+    //servo1.write(90);
+    //servo2.write(90);
+   }
+    
+//    if(PS3.getButtonClick(PS)) {
+//      PS3.disconnect();
+//   }
+    
+    
 }
